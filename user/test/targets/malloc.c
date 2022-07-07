58a7ca1301fc8939e7f8051219a6324373077e7d
powcoder代写 <powcoder@163.com>
2022-07-07 14:03:27 +0800

    程序代写 加微信 powcoder

---
+++ user/test/targets/malloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+void on_ctrl_c(void) {}
+
+#define PRIV_STR "Hey this is private!"
+
+int main(void)
+{
+	if (signal(SIGINT, on_ctrl_c) == SIG_ERR) {
+		perror("signal");
+		return 1;
+	}
+
+	char *area = malloc(sizeof(PRIV_STR));
+
+	if (area == NULL) {
+		perror("malloc");
+		return 1;
+	}
+	strcpy(area, PRIV_STR);
+	printf("0x%lx\n", (unsigned long)area);
+
+	pause();
+
+	printf("%s\n", area);
+	free(area);
+}
---
+++ user/test/targets/strlit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+void on_ctrl_c(void) {}
+
+#define PRIV_STR "Hey this is private!"
+
+int main(void)
+{
+	if (signal(SIGINT, on_ctrl_c) == SIG_ERR) {
+		perror("signal");
+		return 1;
+	}
+
+	char *area = PRIV_STR;
+
+	printf("0x%lx\n", (unsigned long)area);
+
+	pause();
+
+	printf("%s\n", area);
+}
